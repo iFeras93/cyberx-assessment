a088c82ddbcd226686f058c20172592d6af51c81
Feras Mohamed <ferasmo7mad@gmail.com>
2024-12-06 02:39:35 +0200

    Chore: define seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\ActivityLog;
+use App\Models\Assessment;
+use App\Models\Survey;
+use App\Models\Task;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +20,24 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+        User::factory(10)->create()->each(function ($user) {
+            // Create assessments for each user
+            Assessment::factory(3)->create(['user_id' => $user->id]);
+
+            // Create surveys for each user
+            Survey::factory(2)->create(['user_id' => $user->id]);
+
+            // Create tasks for each user
+            Task::factory(5)->create(['user_id' => $user->id]);
+
+            // Create activity logs for each user
+            ActivityLog::factory(7)->create(['user_id' => $user->id]);
+        });
+
     }
 }
